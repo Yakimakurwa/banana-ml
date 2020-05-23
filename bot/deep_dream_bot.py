442302753c7a72ae01bb80169c7e5eb7eeea595c
Yakimakurwa <katya_game.com>
2020-05-23 20:12:31 +0000

    hope bot is ready now

--- bot/deep_dream_bot.py
+++ bot/deep_dream_bot.py
@@ -19,6 +19,9 @@ import logging
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from predict_model import Predictor
 
+from skimage.io import imread
+import skimage
+
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
@@ -42,7 +45,10 @@ def start(update, context):
 def help(update, context):
     """Send a message when the command /help is issued."""
     update.message.reply_text('Ohh, you need my help. Alright.')
-   
+    update.message.reply_text('I was made for changing your photo, so send me one and I will
+      make it look stranger.
+      Also you can tell me, which filter you want me to use.')
+    upate.message.reply_text('Also I can make echo, huh')
 
 def error(update, context):
     """Log Errors caused by Updates."""
@@ -64,17 +70,17 @@ def get_photo(update, context):
     # get photo file
     photo_file = update.message.photo[-1].get_file()
     # save photo
+    photo_file = Image.fromarray(skimage.img_as_ubyte(photo_file)).resize((512, 512))
     photo_file.download('user_photo.jpg')
     logger.info("Photo of %s: %s", user.first_name, 'user_photo.jpg')
     update.message.reply_text('Gorgeous! Got your photo')
-    
+   
+
     # load saved photo
-    new_photo = open('user_photo.jpg', 'rb')
+    new_photo = open('result.jpg', 'w')
 
     # send photo
     context.bot.send_photo(chat_id=update.effective_chat.id, photo=new_photo)
-    result = predictor.get_image_predict('user_photo.jpg')
-    context.bot.send_message(chat_id=update.effective_chat.id, text=result)
 
 def main():
     """Start the bot."""
